53c4e227a9cbb100ae1fff8624ead63edc99a1cb
Alexey Chernov <alexey@sunbuh.ru>
2016-12-14 16:53:38 +0300

    Modified new card view

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,5 @@
 class Card < ApplicationRecord
-    validates :original_text, presence: true, uniqueness: true, length: { minumum: 2 }
-    validates :original_text, presence: true, length: { minumum: 2 }
+    validates :original_text, presence: true, uniqueness: true, length: { minimum: 2 }
+    validates :translated_text, presence: true, length: { minimum: 2 }
     validates :review_date, presence: true
 end
