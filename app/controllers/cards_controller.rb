1ea70e55a2fc10e2232b63429eb0a0e3dc33b088
Alexey Chernov <alexey@sunbuh.ru>
2016-12-14 16:47:59 +0300

    Added validations to Card model and view to create cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,10 +3,26 @@ class CardsController < ApplicationController
 		@cards = Card.all
 	end
 
+	def new
+		@card = Card.new
+	end
+
+	def create
+		@card = Card.new(card_params)
+ 
+		@card.save
+		redirect_to @card
+  	end
+
 	def destroy
 		@card = Card.find(params[:id])
 		@card.destroy
 
 		redirect_to cards_path
 	end
+
+	private
+		def card_params
+			params.require(:card).permit(:original_text, :translated_text)
+  		end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,5 @@
 class Card < ApplicationRecord
+    validates :original_text, presence: true, uniqueness: true, length: { minumum: 2 }
+    validates :original_text, presence: true, length: { minumum: 2 }
+    validates :review_date, presence: true
 end
