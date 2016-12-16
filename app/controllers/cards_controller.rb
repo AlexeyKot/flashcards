d3c9a62401f5069a6f4a7732a3749e083a648f71
Alexey Chernov <alexey@sunbuh.ru>
2016-12-16 12:55:11 +0300

    Added edit view and implemented its logic

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -7,12 +7,25 @@ class CardsController < ApplicationController
 		@card = Card.new
 	end
 
+	def edit
+		@card = Card.find(params[:id])
+	end
+
 	def create
 		@card = Card.new(card_params)
  
 		@card.save
 		redirect_to @card
   	end
+
+	def update
+		@card = Card.find(params[:id])
+		if @card.update(card_params)
+			redirect_to @card
+		else
+			render 'edit'
+		end
+	end
 
 	def show
 		@card = Card.find(params[:id])
