364cfdf27362eeb32288b873c6defa773c2ef040
Alexey Chernov <shadmas@gmail.com>
2017-01-02 15:32:42 +0000

    added mechanize gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "nokogiri"
+require "open-uri"
+
+url = "http://www.languagedaily.com/learn-german/vocabulary/common-german-words-7"
+page = open(url)
+parse_page = Nokogiri::HTML(page)
+counter = 0
+parse_page.css('.jsn-article-content').css('tbody').css('tr.rowA, tr.rowB').each do |a|
+	english_word = a.css('td[3]').text.gsub(160.chr("UTF-8"),"")
+	german_word = a.css('td[2]').text.gsub(160.chr("UTF-8"),"")
+	if !english_word.to_s.empty?
+		Card.create(original_text: german_word, translated_text: english_word)
+		counter+=1
+	end
+end
+if counter > 0
+	puts "Added " + counter.to_s + " records to database"
+else
+	puts "Something went wrong or nothing to add to database"
+end
+
