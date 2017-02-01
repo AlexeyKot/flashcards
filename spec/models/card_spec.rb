69e26f7a43ff78efd380df59177cca7251b771f1
Alexey Chernov <shadmas@gmail.com>
2017-02-01 11:36:54 +0000

    Saving current state

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-describe Card do
+describe Card, type: :model do 
     context "Check validators" do
         it { should validate_presence_of(:original_text) }
         it { should validate_presence_of(:translated_text) }
         it { should validate_uniqueness_of(:original_text) }
-        it { should ensure_length_of(:original_text).is_at_least(2) }
-        it { should ensure_length_of(:translated_text).is_at_least(2) }
-    end
+        it { should validate_length_of(:original_text).is_at_least(2) }
+        it { should validate_length_of(:translated_text).is_at_least(2) }
+    end    
 
-    
+
 end
