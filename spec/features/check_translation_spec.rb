678550fd85923b57041b6dba2ab13e63935335af
Alexey Chernov <shadmas@gmail.com>
2017-02-08 12:20:20 +0000

    Fixed tests

--- spec/features/check_translation_spec.rb
+++ spec/features/check_translation_spec.rb
@@ -1,12 +1,20 @@
 require 'rails_helper'
 
-feature "User checks translation", :type => :feature do
-  let!(:card) { create(:card, original_text: "шапка", translated_text: "hat", review_date: (Date.today - 3.days))}
+feature "Check translation", :type => :feature do
+  let!(:card) { Card.create(original_text: "hat", translated_text: "шапка", review_date: (Date.today - 3.days))}
   scenario "successfully" do
     visit root_path
-    fill_in "Answer", with: "hat"
+    fill_in "check_answer", with: "hat"
     click_on "Проверить"
 
     expect(page).to have_content "Правильный ответ"
+  end
+
+  scenario "fail" do
+    visit root_path
+    fill_in "check_answer", with: "hut"
+    click_on "Проверить"
+
+    expect(page).to have_content "неправильно"
   end
 end
