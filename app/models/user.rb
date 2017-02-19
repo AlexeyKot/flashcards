e62704b8fc277f4db9c6b0ef898e882b1b76c915
Alexey Chernov <shadmas@gmail.com>
2017-02-19 09:09:55 +0000

    Added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  validates :password, length: { minimum: 3 }, confirmation: true
+  validates :email, length: { minimum: 8 }, uniqueness: true
 end
