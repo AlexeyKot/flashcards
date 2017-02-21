15fb10ec5699060cff4243a56b041c86b98b3d9d
Alexey Chernov <shadmas@gmail.com>
2017-02-21 19:25:03 +0000

    Fixed new session

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ Rails.application.routes.draw do
 		end
 	end
 	resources :users, only: [:new, :create]
+	resources :sessions, only: [:new, :create, :destroy]
 	root 'cards#random'
 	get 'static_pages/home'
 	get '/sign_up', to: 'users#new', as: :sign_up
+	get '/log_in', to: 'sessions#new', as: :log_in
+	delete '/log_out', to: 'sessions#destroy', as: :log_out
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
