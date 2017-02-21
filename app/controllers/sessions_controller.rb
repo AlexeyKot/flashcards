3f41b8f59cc43aeafae5cfff810769b143c5258b
Alexey Chernov <shadmas@gmail.com>
2017-02-21 09:22:39 +0000

    Added sessions methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+	def new
+	end
+
+	def create
+		if login(params[:email], params[:password])
+			flash[:success] = 'С возвращением!'
+			redirect_to root_path
+		else
+			flash.now[:warning] = 'Неправильный email или пароль'
+			render 'new'
+		end
+	end
+
+	def destroy
+		logout
+		flash[:success] = 'До свидания!'
+		redirect_to log_in_path
+	end
 end
